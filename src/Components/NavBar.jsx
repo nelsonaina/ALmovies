2213021603ce456ab26ec20b661a2f638160f8f0
nelsonaina <ainainioluwa090@gmail.com>
2026-04-23 13:05:00 -0400

    Api Key Fix

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -3,22 +3,20 @@ import "../css/Navbar.css";
 
 function NavBar() {
     return (
-        <>
-            <nav className="navbar">
-                <div className="navbar-brand">
-                    <Link to="/">Movie App</Link>
-                </div>
+        <nav className="navbar">
+            <div className="navbar-brand">
+                <Link to="/">Movie App</Link>
+            </div>
 
-                <div className="navbar-links">
-                    <Link to="/" className="nav-link">
-                        Home
-                    </Link>
-                    <Link to="/favorites" className="nav-link">
-                        Favorites
-                    </Link>
-                </div>
-            </nav>
-        </>
+            <div className="navbar-links">
+                <Link to="/" className="nav-link">
+                    Home
+                </Link>
+                <Link to="/favorites" className="nav-link">
+                    Favorites
+                </Link>
+            </div>
+        </nav>
     );
 }
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -42,7 +42,7 @@ function Home() {
             setLoading(false);
         }
 
-        setSearchQuery("");
+        // setSearchQuery("");
     };
 
     return (
